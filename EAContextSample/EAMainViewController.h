802fa897506bb4598b801854b9c6336f0da7a385
Steven <steven.lee@atomaxinc.com>
2015-08-12 16:17:15 +0800

    Project TA (Taekwondo) first commit

--- EAContextSample/EAMainViewController.h
+++ EAContextSample/EAMainViewController.h
@@ -9,6 +9,12 @@
 #import "EAFlipsideViewController.h"
 
 @interface EAMainViewController : UIViewController <EAFlipsideViewControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIView *thisView;
+@property (strong, nonatomic) IBOutlet UIButton *startBtn;
+@property (strong, nonatomic) IBOutlet UISlider *senSlider;
+@property (strong, nonatomic) IBOutlet UISlider *hitSlider;
+@property (strong, nonatomic) IBOutlet UISlider *intervalSlider;
+/*
 @property (strong, nonatomic) IBOutlet UILabel *accelValueLb_x;
 @property (strong, nonatomic) IBOutlet UILabel *accelValueLb_y;
 @property (strong, nonatomic) IBOutlet UILabel *accelValueLb_z;
@@ -20,9 +26,16 @@
 @property (strong, nonatomic) IBOutlet UILabel *gravityValueLb_x;
 @property (strong, nonatomic) IBOutlet UILabel *gravityValueLb_y;
 @property (strong, nonatomic) IBOutlet UILabel *gravityValueLb_z;
-
+*/
 @property (strong, nonatomic) IBOutlet UILabel *uAccelValueLb_x;
 @property (strong, nonatomic) IBOutlet UILabel *uAccelValueLb_y;
 @property (strong, nonatomic) IBOutlet UILabel *uAccelValueLb_z;
+
+@property (strong, nonatomic) IBOutlet UILabel *rtSensitivityLb;
+@property (strong, nonatomic) IBOutlet UILabel *mtSensitivityLb;
+@property (strong, nonatomic) IBOutlet UILabel *intervalLb;
+
+-(void) startMeasure:(id)sender;
+-(void) startIdle:(id)sender;
 
 @end
--- EAContextSample/EAMainViewController.m
+++ EAContextSample/EAMainViewController.m
@@ -15,6 +15,16 @@
 
 @implementation EAMainViewController
 
+int ta_stat = 0;
+float ta_th_sen = 0.25;
+float ta_th_hit = 2.0;
+AMXVector3 ta_gravity_ref = {0};
+NSTimeInterval startTime;
+NSTimeInterval reactionTime;
+NSTimeInterval motionTime;
+NSTimer *triggerTimer;
+NSTimeInterval triggerInterval = 10.0;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -23,6 +33,20 @@
     self.context = [[EAContext alloc] initWithSport:EASportTypeRAWData/*|EASportTypeBadminton*/ andConnectionType:EAConnectionTypeBLE];
 
     self.context.delegate = self;
+    
+    self.thisView.backgroundColor = [UIColor blackColor];
+    self.senSlider.maximumValue = 0.8;
+    self.senSlider.minimumValue = 0.1;
+    self.senSlider.value = ta_th_sen;
+
+    self.hitSlider.maximumValue = 5.0;
+    self.hitSlider.minimumValue = 0.5;
+    self.hitSlider.value = ta_th_hit;
+    
+    self.intervalSlider.maximumValue = 20.0;
+    self.intervalSlider.minimumValue = 4.0;
+    self.intervalSlider.value = triggerInterval;
+
     
     double delayInSeconds = 2.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
@@ -94,24 +118,41 @@
         
         [userinfo[kEASportResultUserAcceleration] getValue:&userAccel];
         
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            self.accelValueLb_x.text = [NSString stringWithFormat:@"%f",accel.x];
-            self.accelValueLb_y.text = [NSString stringWithFormat:@"%f",accel.y];
-            self.accelValueLb_z.text = [NSString stringWithFormat:@"%f",accel.z];
-            
-            self.gyroValueLb_x.text = [NSString stringWithFormat:@"%f",gyro.x];
-            self.gyroValueLb_y.text = [NSString stringWithFormat:@"%f",gyro.y];
-            self.gyroValueLb_z.text = [NSString stringWithFormat:@"%f",gyro.z];
-            
-            self.gravityValueLb_x.text = [NSString stringWithFormat:@"%f",gravity.x];
-            self.gravityValueLb_y.text = [NSString stringWithFormat:@"%f",gravity.y];
-            self.gravityValueLb_z.text = [NSString stringWithFormat:@"%f",gravity.z];
-            
-            self.uAccelValueLb_x.text = [NSString stringWithFormat:@"%f",userAccel.x];
-            self.uAccelValueLb_y.text = [NSString stringWithFormat:@"%f",userAccel.y];
-            self.uAccelValueLb_z.text = [NSString stringWithFormat:@"%f",userAccel.z];
+        
+        // project ta
+        ta_gravity_ref.x = (ta_gravity_ref.x * 0.8) + (gravity.x * 0.2);
+        ta_gravity_ref.y = (ta_gravity_ref.y * 0.8) + (gravity.y * 0.2);
+        ta_gravity_ref.z = (ta_gravity_ref.z * 0.8) + (gravity.z * 0.2);
+        
+        AMXVector3 ta_diff = {0};
+        ta_diff.x = fabsf(ta_gravity_ref.x - gravity.x);
+        ta_diff.y = fabsf(ta_gravity_ref.y - gravity.y);
+        ta_diff.z = fabsf(ta_gravity_ref.z - gravity.z);
+        float diff = sqrtf((ta_diff.x * ta_diff.x) + (ta_diff.y * ta_diff.y) + (ta_diff.z * ta_diff.z));
+        
+        float pureAccel = sqrtf((userAccel.x * userAccel.x) + (userAccel.y * userAccel.y) + (userAccel.z * userAccel.z));
 
+        
+        // State Transition
+        if (ta_stat == 1) { // In measuring reaction time
+            reactionTime = fabs([[NSDate date] timeIntervalSince1970] - startTime);
+            if (diff > ta_th_sen) {
+                ta_stat = 2;
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.thisView.backgroundColor = [UIColor whiteColor];
+                });
+            }
+        } else if (ta_stat == 2) { // In motion
+            motionTime = fabs([[NSDate date] timeIntervalSince1970] - startTime - reactionTime);
+            if ((pureAccel > ta_th_hit)&&(motionTime > 0.1)) {
+                [self startIdle:nil];
+            }
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+//            self.uAccelValueLb_x.text = [NSString stringWithFormat:@"stat %d",ta_stat];
+            self.uAccelValueLb_y.text = [NSString stringWithFormat:@"RT: %.0f ms",(float)(reactionTime * 1000)];
+            self.uAccelValueLb_z.text = [NSString stringWithFormat:@"MT: %.0f ms",(float)(motionTime * 1000)];
         });
         
     }
@@ -122,6 +163,85 @@
 {
     printf("\n error in MainWindowViewcontroller -> %s",[[error description] UTF8String]);
 }
+
+- (void)startTimer {
+    int rand = arc4random_uniform(100);
+    NSTimeInterval randInterval = triggerInterval + ((double)rand / 50.0);
+    triggerTimer = [NSTimer scheduledTimerWithTimeInterval:randInterval target:self selector:@selector(startMeasure:) userInfo:nil repeats:YES];
+}
+
+
+- (void) stopTimer{
+    [triggerTimer invalidate];
+    triggerTimer = nil;
+}
+
+
+
+-(void) startMeasure:(id)sender {
+    ta_stat = 1;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.thisView.backgroundColor = [UIColor greenColor];
+        startTime = [[NSDate date] timeIntervalSince1970];
+        reactionTime = 0.0;
+        motionTime = 0.0;
+        [self.uAccelValueLb_y setText:@""];
+        [self.uAccelValueLb_z setText:@""];
+    });
+}
+-(void) startIdle:(id)sender {
+    ta_stat = 0;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.thisView.backgroundColor = [UIColor blackColor];
+    });
+    
+}
+
+
+- (IBAction)didChangeSenSlider:(id)sender {
+    ta_th_sen = self.senSlider.value;
+}
+
+- (IBAction)didChangeHitSlider:(id)sender {
+    ta_th_hit = self.hitSlider.value;
+}
+
+- (IBAction)didChangeIntervalSlider:(id)sender {
+    triggerInterval = self.intervalSlider.value;
+}
+
+- (IBAction)didClickStartBtn:(UIButton *)sender {
+    
+    if(self.startBtn.isSelected) {
+        [self.startBtn setSelected:NO];
+        [self stopTimer];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.startBtn setTitle:@"Start" forState:UIControlStateNormal];
+            [self.hitSlider setHidden:NO];
+            [self.senSlider setHidden:NO];
+            [self.intervalSlider setHidden:NO];
+            [self.rtSensitivityLb setHidden:NO];
+            [self.mtSensitivityLb setHidden:NO];
+            [self.intervalLb setHidden:NO];
+            
+        });
+    } else {
+        [self.startBtn setSelected:YES];
+        [self startTimer];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.startBtn setTitle:@"Reset" forState:UIControlStateSelected];
+            [self.hitSlider setHidden:YES];
+            [self.senSlider setHidden:YES];
+            [self.intervalSlider setHidden:YES];
+            [self.intervalLb setHidden:YES];
+            [self.rtSensitivityLb setHidden:YES];
+            [self.mtSensitivityLb setHidden:YES];
+
+        });
+    }
+    
+}
+
 
 
 
